6808ff3d59639a20800645d856d1af34fef24879
MORENO Abel <abel.moreno@wfp.org>
2016-06-02 10:30:13 -0500

    Archivos de Wordpress

---
+++ wp-content/themes/Arcadia WordPress Theme Package (1.6)/Child Theme Skeleton/Arcadia_Child_Theme/functions.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+/* Replacement Functions
+============================================================ */
+
+
+
+/* Your Replacement Function
+------------------------------------------------------------ */
+
+function ct_your_replacement_function() {
+  
+}
+
+
+
+
+
+
+
+
+
+
+/* Additional Functions
+============================================================ */
+
+
+
+/* Your Additional Function
+------------------------------------------------------------ */
+
+function ct_your_additional_function() {
+  
+}
+
+  
+
+
+
+
+
+
+
+
+?>
